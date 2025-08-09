652c39a8668bb735c6bd9473bffc63f14813d64b
Visar Uruqi <visar.uruqi@gmail.com>
2025-08-09 12:08:40 +0200

    feat: add sidebar title component

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,6 +1,13 @@
 import DefaultTheme from 'vitepress/theme'
+import { h } from 'vue'
+import SidebarTitle from './SidebarTitle.vue'
 import './custom.css'
 
 export default {
-  ...DefaultTheme
+  ...DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'sidebar-top': () => h(SidebarTitle)
+    })
+  }
 }
