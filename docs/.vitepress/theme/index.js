a326715f01cb6fd9b923b46d476d6ff36356a5dc
Visar Uruqi <visar.uruqi@gmail.com>
2025-08-11 17:59:58 +0200

    feat: add section outline

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,13 +1,15 @@
 import DefaultTheme from 'vitepress/theme'
 import { h } from 'vue'
 import SidebarTitle from './SidebarTitle.vue'
+import SectionOutline from './SectionOutline.vue'
 import './custom.css'
 
 export default {
   ...DefaultTheme,
   Layout() {
     return h(DefaultTheme.Layout, null, {
-      'sidebar-nav-before': () => h(SidebarTitle)
+      'sidebar-nav-before': () => h(SidebarTitle),
+      'aside-outline-after': () => h(SectionOutline)
     })
   }
 }
